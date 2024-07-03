69fdeb601a2ade01074e74e3aca554b29ef30495
Dmitriy <dmitriytuz123@gmail.com>
2024-07-03 21:15:42 +0300

    change the type of status field in an enum

--- src/schemas/task/task.schema.ts
+++ src/schemas/task/task.schema.ts
@@ -6,15 +6,26 @@ import {Project} from "@src/schemas/project/project.schema";
 
 export type TaskDocument = HydratedDocument<Task>;
 
+export enum TaskStatus {
+  New = 'New',
+  InProgress = 'In Progress',
+  Complete = 'Complete'
+}
+
+
 @Schema({ timestamps: true })
 export class Task {
   @Prop()
   @ApiProperty({ example: 'Title1', description: 'Task title' })
   title: string;
 
-  @Prop({ default: 'New' })
-  @ApiProperty({ example: 'New', description: 'Task status' })
-  status: string;
+  @Prop({ enum: TaskStatus, default: TaskStatus.New })
+  @ApiProperty({ example: TaskStatus.New, description: 'Task status', enum: TaskStatus })
+  status: TaskStatus;
+
+  // @Prop({ default: 'New' })
+  // @ApiProperty({ example: 'New', description: 'Task status' })
+  // status: string;
 
   @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Project'})
   projectId: Project
